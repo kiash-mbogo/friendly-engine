83ed4c76a14a3c3b0cc5268502d4ab477cb7177e
Alvin Mbogo <mbogoalvin9@gmail.com>
2026-02-10 19:48:15 +0300

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        byte mynum = 20;
+        system.out.println("mynum");
     }
 }
+
